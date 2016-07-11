7061cc213b5f96b8a175ebdf84640017de08c77f
Daniel Beyer <Daniel.Beyer@cgi.com>
2016-07-11 21:37:50 +0200

    initial load
    Load f. Spesen Freigabe

---
+++ SpesenFreigabe/src/main/java/org/camunda/bpmn/spesenfreigabe/SpesenFreigabeApplication.java
@@ -0,0 +1,10 @@
+package org.camunda.bpmn.spesenfreigabe;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Loan Approval App")
+public class SpesenFreigabeApplication extends ServletProcessApplication {
+  // empty implementation
+}
+
